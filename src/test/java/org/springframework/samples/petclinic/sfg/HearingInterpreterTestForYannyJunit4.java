7f5d2b18978143799e331af00b8a5fd5a87b8587
Cham <runningpaipai@gmail.com>
2024-02-17 22:16:37 +0800

    Feat: Yanny Testing using JUnit 4

--- src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTestForYannyJunit4.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTestForYannyJunit4.java
@@ -1,5 +1,6 @@
 package org.springframework.samples.petclinic.sfg;
 
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,10 +10,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-// Bring up spring context
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {BaseConfig.class, LaurelConfig.class})
-public class HearingInterpreterTestJUnit4 {
+@ContextConfiguration(classes = {BaseConfig.class, YannyConfig.class})
+public class HearingInterpreterTestForYannyJunit4 {
+
 
 
     @Autowired
@@ -27,7 +28,6 @@ public class HearingInterpreterTestJUnit4 {
     public void whatIHeard() {
         String word = hearingInterpreter.whatIHeard();;
 
-        assertThat(word).isEqualTo("Laurel");
+        assertThat(word).isEqualTo("Yanny");
     }
-
 }
---
+++ src/test/java/org/springframework/samples/petclinic/sfg/YannyConfig.java
@@ -0,0 +1,15 @@
+package org.springframework.samples.petclinic.sfg;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class YannyConfig {
+
+    @Bean
+    YannyWordProducer producer() {
+        return new YannyWordProducer();
+    }
+
+
+}
